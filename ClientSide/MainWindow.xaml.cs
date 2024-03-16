dcf7c3ba424e89c339cac55df732ae9b6eb3cb2e
Penahli Ibrahim <penahliibrahim58@gmail.com>
2024-03-16 23:47:13 +0400

    Commitment 1

---
+++ ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ClientSide;
+
+public partial class MainWindow : Window
+{
+
+    string checkIP = Directory.GetCurrentDirectory().ToString() == "C:\\Users\\asus\\source\\repos\\NetworkScreenSharing\\ClientSide\\bin\\Debug\\net6.0-windows" ? "192.168.56.1" : "127.0.0.1";
+    public UdpClient client;
+    public IPAddress remoteIP;
+    public int remotePort;
+    public IPEndPoint remoteEP;
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        remoteIP = IPAddress.Parse(checkIP);
+        remotePort = 27001;
+        remoteEP = new IPEndPoint(remoteIP, remotePort);
+
+        client = new UdpClient();
+    }
+
+
+
+
+    public async Task<BitmapImage?> ByteToImageAsync(byte[]? imageData)
+    {
+        var image = new BitmapImage();
+
+        image.BeginInit();
+
+        image.StreamSource = new MemoryStream(imageData);
+        image.CacheOption = BitmapCacheOption.OnLoad;
+
+        image.EndInit();
+
+        return image;
+    }
+
+    private async void StartScreenSharing(object sender, RoutedEventArgs e)
+    {
+        var maxLen = ushort.MaxValue - 29;
+        var len = 0;
+        var buffer = new byte[maxLen];
+        await client.SendAsync(buffer, buffer.Length, remoteEP);
+
+        var list = new List<byte>();
+        while (true)
+        {
+            do
+            {
+                try
+                {
+                    var result = await client.ReceiveAsync();
+
+                    buffer = result.Buffer;
+                    len = buffer.Length;
+                    list.AddRange(buffer);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            } while (len == maxLen);
+
+            var image = await ByteToImageAsync(list.ToArray());
+
+            if (image is not null)
+                ScreenShare.Source = image;
+
+            list.Clear();
+        }
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,59 @@
+﻿#nullable disable
+
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Windows.Forms;
+
+string checkIP = Directory.GetCurrentDirectory().ToString() == "C:\\Users\\asus\\source\\repos\\NetworkScreenSharing\\ServerSide\\bin\\Debug\\net6.0" ? "192.168.56.1" : "127.0.0.1";
+
+var ip = IPAddress.Parse(checkIP);
+var port = 27001;
+
+var listenerEP = new IPEndPoint(ip, port);
+var listener = new UdpClient(listenerEP);
+
+
+IPEndPoint? remoteEP = null;
+while (true)
+{
+    var result = await listener.ReceiveAsync();
+    if (result != null)
+    {
+        Console.WriteLine($"{result.RemoteEndPoint} Connected To Server.");
+        _ = Task.Run(async () =>
+        {
+            IPEndPoint? remoteEP = result.RemoteEndPoint;
+
+            while (true)
+            {
+                var screenImage = await TakeScreenShotAsync();
+                var imgBytes = await ImageToByteAsync(screenImage);
+                var chunks = imgBytes?.Chunk(ushort.MaxValue - 29);
+                foreach (var chunk in chunks!)
+                    await listener.SendAsync(chunk, chunk.Length, remoteEP);
+            }
+        });
+    }
+}
+
+async Task<Image?> TakeScreenShotAsync()
+{
+    var width = Screen.PrimaryScreen.Bounds.Width;
+    var height = Screen.PrimaryScreen.Bounds.Height;
+    Bitmap? bitmap = new Bitmap(width, height);
+    using Graphics graphics = Graphics.FromImage(bitmap);
+    graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+    return bitmap;
+}
+
+async Task<byte[]?> ImageToByteAsync(Image? image)
+{
+    using MemoryStream ms = new MemoryStream();
+    image?.Save(ms, ImageFormat.Jpeg);
+
+    return ms.ToArray();
+}
